39b2d7e6cbc8224eaf80c8f922f0d970b695bdea
dafalagi <dafarizky34@gmail.com>
2023-08-03 00:25:47 +0700

    update 3/8/23

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False') == 'True'
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1'])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost')
 
 
 # Application definition
