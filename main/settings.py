dc43c744d07d17844b89b3cef3e1cc82e64b6a2e
dafalagi <dafarizky34@gmail.com>
2023-08-02 23:44:59 +0700

    update 2/8/23

--- main/settings.py
+++ main/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 from django.core.management.utils import get_random_secret_key
-import os, dj_database_url
+import os, dj_database_url, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
