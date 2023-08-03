83ede7f92cc37ca8b2970b05b651cac46beb5466
dafalagi <dafarizky34@gmail.com>
2023-08-03 14:41:25 +0700

    update 3/8/23

--- main/settings.py
+++ main/settings.py
@@ -71,8 +71,8 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, 'templates'),
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
